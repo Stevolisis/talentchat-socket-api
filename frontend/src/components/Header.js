81dc3e02b18d706ed2da391b75fcaca04e0307c6
Steven Joseph <stevolisisjoseph@gmail.com>
2023-04-21 12:56:33 +0100

    Header Stylings

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,9 +4,9 @@ export default function Header(){
 
     return(
         <>
-            <header>
-                <h1>TalentVideo</h1>
-                <button>Create Room</button>
+            <header className="bg-bgPrimary text-txtTertiary p-5 flex justify-between items-center">
+                <h1 className="font-[AmazingKids] text-3xl">TalentVideo</h1>
+                <button className="font-[PoppinsMedium] text-sm py-3 px-5 bg-bgSecondary rounded">Create Room</button>
             </header>
         </>
     )
