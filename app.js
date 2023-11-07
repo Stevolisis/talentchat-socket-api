5002b2dbffd76e12c62e6e37e5835ea02b345f6f
Steven Joseph <stevolisisjoseph@gmail.com>
2023-11-07 14:25:53 +0100

    app.js socket for client folder react chat

---
+++ app.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const http=require('http');
+const app=express();
+const server=http.createServer(app);
+const io=require('socket.io')(server,{
+    cors:{
+        origin:'http://localhost:5173',
+        method:['GET','POST']
+    }
+});
+
+server.listen(80,()=>{console.log('Server running at PORT 80')})
+
+io.on("connection",(socket)=>{
+    console.log(socket);
+});
