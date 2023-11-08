ebbcd58afadec7764bf685c3644bfd6ad5d8bdfb
Steven Joseph <stevolisisjoseph@gmail.com>
2023-11-08 21:13:52 +0100

    process.env changed to import.meta because its vite, but its showing undefined

--- client/src/containers/index.jsx
+++ client/src/containers/index.jsx
@@ -3,8 +3,13 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import JoinRoom from "../components/JoinRoom";
 import Chats from "../components/Chats";
+import {io} from 'socket.io-client';
 
+const socket=io.connect(import.meta.VITE_SOCKET_HOST);
+console.log('1111: ',import.meta.VITE_SOCKET_HOST);
 export default function Index(){
+    
+    console.log('2222: ',import.meta.VITE_SOCKET_HOST);
 
     return(
         <>
