966718316b7b61d366337d62cd0d1ab68e4188df
Steven Joseph <stevolisisjoseph@gmail.com>
2023-11-11 10:19:14 +0100

    Updated join room by adding room select

--- client/src/components/JoinRoom.jsx
+++ client/src/components/JoinRoom.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 
-export default function JoinRoom(){
+export default function JoinRoom({setVerified,socket}){
+    const [userName,setuserName] = useState('');
+    const [room,setRoom] = useState('');
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        socket.emit('setup',{name:userName,room:room});
+    }
 
     return(
         <>
             <div className="w-[100vw] z-10 h-[100vh] fixed bg-bgSecondary flex justify-center items-center">
-                <form className="px-5 py-5 flex flex-col justify-center items-center bg-brSecondary w-[35vw] rounded-md">
-                    <input type='text' className="font-[PoppinsLight] text-xs bg-slate-200 w-full border-2 py-2 px-3 border-brTertiary outline-none rounded-md" placeholder="Room Id"/>
+                <form onSubmit={(e)=>handleSubmit(e)} className="px-5 py-5 flex flex-col justify-center items-center bg-brSecondary w-[35vw] rounded-md">
+                    <input value={userName} onChange={(e)=>setuserName(e.target.value)} name="user" type='text' className="mt-2 font-[PoppinsLight] text-xs bg-slate-200 w-full border-2 py-2 px-3 border-brTertiary outline-none rounded-md" placeholder="UserName"/>
+                    <select value={room} onChange={(e)=>setRoom(e.target.value)} name="room" type='text' className="mt-2 font-[PoppinsLight] text-xs bg-slate-200 w-full border-2 py-2 px-3 border-brTertiary outline-none rounded-md" placeholder="Room">
+                        <option>Javascript</option>
+                        <option>PHP</option>
+                        <option>Python</option>
+                        <option>C++</option>
+                    </select>
                     <button className="font-[PoppinsRegular] mt-2 px-20 py-2 rounded-md text-xs bg-brTertiary text-txtSecondary">Join Room</button>
                 </form>
             </div>
--- client/src/containers/index.jsx
+++ client/src/containers/index.jsx
@@ -4,10 +4,12 @@ import Header from "../components/Header";
 import JoinRoom from "../components/JoinRoom";
 import Chats from "../components/Chats";
 import {io} from 'socket.io-client';
+import { useState } from "react";
 
 let socket=io.connect(import.meta.env.VITE_SOCKET_HOST);
 
 export default function Index(){
+    const [verified,setVerified] = useState(false);
     socket.on("connect", () => {
         console.log('ggg',socket.id);
         localStorage.setItem('token',socket.id);
@@ -15,10 +17,12 @@ export default function Index(){
     socket.on('me',(arg)=>{
         console.log(arg)
     });
+
+
     
     return(
         <>
-            {/* <JoinRoom/> */}
+            {!verified && <JoinRoom setVerified={setVerified} socket={socket}/>}
             <Header/>
             <div className="flex bg-bgSecondary h-[86vh]">
                 <Participants/>
