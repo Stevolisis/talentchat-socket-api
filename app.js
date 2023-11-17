c4d776196503524a2c39325b81365d6e8fccd31d
Steven Joseph <stevolisisjoseph@gmail.com>
2023-11-17 22:11:13 +0100

    app.js update: io join group commented

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const server=http.createServer(app);
 // console.log('opop',process.env.CORS_ALLOW)
 const io=require('socket.io')(server,{
     cors:{
-        // origin:'http://localhost:5173',
-        origin:process.env.CORS_ALLOW,
+        origin:'http://localhost:5173',
+        // origin:process.env.CORS_ALLOW,
         method:['GET','POST']
     }
 });
@@ -20,21 +20,21 @@ const botName = 'TalentChat Bot';
 io.on("connection",(socket)=>{
     socket.emit("me",socket.id);
 
-    socket.on('join-room',(args)=>{
-        const user = userJoin(socket.id,args.name,args.room);
-        socket.join(user.room);
-        io.to(user.room).emit('room-users',getRoomUsers(user.room));
-        io.to(user.room).emit('getRoom',user.room);
-        socket.emit('message',formatMessage(socket.id,botName,'Welcome to TalentChat'));
-        socket.broadcast.to(user.room).emit('message', formatMessage(socket.id, botName, `${user.userName} has joined the chat`));
+    // socket.on('join-room',(args)=>{
+    //     const user = userJoin(socket.id,args.name,args.room);
+    //     socket.join(user.room);
+    //     io.to(user.room).emit('room-users',getRoomUsers(user.room));
+    //     io.to(user.room).emit('getRoom',user.room);
+    //     socket.emit('message',formatMessage(socket.id,botName,'Welcome to TalentChat'));
+    //     socket.broadcast.to(user.room).emit('message', formatMessage(socket.id, botName, `${user.userName} has joined the chat`));
 
-        socket.on('group-chat',(msg)=>{
-            io.to(user.room).emit('message', formatMessage(user.id, user.userName, msg));
-        });
-    });
+    //     socket.on('group-chat',(msg)=>{
+    //         io.to(user.room).emit('message', formatMessage(user.id, user.userName, msg));
+    //     });
+    // });
 });
 
 
 app.get('/',(req,res)=>{
-    res.send('Socket backend');
+    res.send('Socket backendedede');
 })
